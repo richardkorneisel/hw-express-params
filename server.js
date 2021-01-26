fd511cf1ef3f9abdaae434d3f0b6730479ec49ec
Rich Korneisel <korneisel@cfu.net>
2021-01-25 19:15:55 -0600

    Commit 1: Greeting express application created.

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ app.get('/greeting', (req, res) => {
     res.send("Hello, stranger");
 });
 
-app.get('/greeting/:name', (req, res) => {​​​​
-    res.send('Wow! Hello there, ' + req.params.name);
-}​​​​);
+app.get('/greeting/:name', (req, res) => {
+    res.send("What's up, " + req.params.name + "!");
+});
 
 app.listen(3000, () => {
     console.log("Server is listening!!!")
