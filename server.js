bb2f77dfdabc367c4078a3c888a718174bd30184
Rich Korneisel <korneisel@cfu.net>
2021-01-25 19:03:26 -0600

    Part One

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get('/greeting', (req, res) => {
+    res.send("Hello, stranger");
+});
+
+app.get('/greeting/:name', (req, res) => {​​​​
+    res.send('Wow! Hello there, ' + req.params.name);
+}​​​​);
+
+app.listen(3000, () => {
+    console.log("Server is listening!!!")
+});
+
