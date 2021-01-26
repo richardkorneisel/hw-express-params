a7e6d59680abe8d16bb7893dffca7cda70b0a2a0
Rich Korneisel <korneisel@cfu.net>
2021-01-25 20:15:26 -0600

    Commit 2: Tip Calculator express application created.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Greetings
 const express = require("express");
 const app = express();
 
@@ -9,7 +10,18 @@ app.get('/greeting/:name', (req, res) => {
     res.send("What's up, " + req.params.name + "!");
 });
 
+//Tip Calculator
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    let total = parseInt(req.params.total);
+    let tipPercentage = parseInt(req.params.tipPercentage);
+    let tip = (tipPercentage/100)*total;
+    let tipString = tip.toString();
+    res.send(tipString);
+});
+
 app.listen(3000, () => {
     console.log("Server is listening!!!")
 });
+
+
 
